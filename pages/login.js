75ed514c3e4d3d22c35b0553d5f5536cba321928
Ditto Alvarez <dittoalvarez@gmail.com>
2021-10-13 22:24:10 +0800

    UI-LoginScreen1

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const login = () => {
+    return (
+        <div>
+            <Head>
+                <title>Heart Token</title>
+                <meta name='keywords' content='heart, token, change the world' />
+            </Head>
+            <h1>login</h1>
+        </div>
+    )
+}
+
+export default login
